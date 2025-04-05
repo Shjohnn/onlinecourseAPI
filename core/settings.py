7136bfb5fdd7b073df2eeba15787b92e767c06a4
Shjohnn <Shjonbek1702@gmail.com>
2025-04-05 16:50:30 +0900

    O'zgarishlar haqida izoh

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c1^jlv$$j251yuqbnkos15@$g@d5u0yv!6*8y-(+4-ag6wadad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
